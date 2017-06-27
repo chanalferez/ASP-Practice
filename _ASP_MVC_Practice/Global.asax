a561bb92109b45557f30a18d7a20c0dcb21a243b
TrainToEkopay <TrainToEkopay@EKOPAY-DEVXXX>
2017-06-27 14:28:19 +0800

    Add project files.

---
+++ _ASP_MVC_Practice/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_ASP_MVC_Practice.MvcApplication" Language="C#" %>
---
+++ _ASP_MVC_Practice/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_ASP_MVC_Practice.Startup))]
+namespace _ASP_MVC_Practice
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
