c38bf975cd41387ec976a2c7fa31d91d1a2c280a
TrainToEkopay <TrainToEkopay@EKOPAY-DEVXXX>
2017-06-29 15:51:55 +0800

    AddingEmployeeTable

---
+++ _ASP_MVC_Practice/Controllers/EmployeeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using _ASP_MVC_Practice.Models;
+using _ASP_MVC_Practice.EDMX;
+
+namespace _ASP_MVC_Practice.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+
+            EmployeePractice emp = new EmployeePractice();
+            tblEmployee emptbl = new tblEmployee();
+
+
+            CRUDExerciseEntities db = new CRUDExerciseEntities();
+
+
+            var em = from e in db.tblEmployees
+                     select e;
+
+            foreach (var data in em) {
+
+            }
+            emp.PCId = emptbl.EmpId;
+
+            return View();
+
+            
+        }
+    }
+}
---
+++ _ASP_MVC_Practice/Models/EmployeePractice.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _ASP_MVC_Practice.Models
+{
+    public class EmployeePractice
+    {
+      
+        public int PCId { get; set; }
+        public long ServerIpAddress { get; set; }
+        public int ServerPort { get; set; }
+        public Nullable<bool> ServerSSL { get; set; }
+        public long createDate { get; set; }
+        public long modifiedDate { get; set; }
+        public string test_Columns { get; set; }
+      
+    }
+}
